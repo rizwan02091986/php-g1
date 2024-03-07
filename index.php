5e68d6c6f9264a2fee5744ab98e1ded4cdb0264e
M.Rizwan <rizwan02091986@gmail.com>
2024-03-07 20:27:40 +0500

    07-03-2024

--- index.php
+++ index.php
@@ -25,5 +25,42 @@
     echo "<br> the mul of $a * $b is :" .$e;
     echo "<br> the div of $a / $b is :" .$f;
     echo "<br> the mod of $a % $b is :" .$g;
+    
+    // in-crement
+
+    $apple = 70;
+    // post increment
+    echo "<br>" .$apple++; //70
+    echo "<br>" .$apple; //71
+    // pre increment
+    echo "<br>" .++$apple; //72
+
+    // post de-crement
+    echo "<br>" .$apple--; //72
+    echo "<br>" .$apple; //71
+
+    // pre de-crement
+    echo "<br>" .--$apple; //70
+
+    $co = 200;
+    echo "<br> $co";
+    $co -=5;
+    echo "<br> $co";
+    $co +=40;
+    echo "<br> $co";
+    $co -=126;
+    echo "<br> $co";
+    $co +=95;
+    echo "<br> $co";
+    $co -=124;
+    echo "<br> $co";
+    $co +=116;
+    echo "<br> $co";
+    $co +=104;
+    echo "<br> $co";
+
+    
+
+
 
 ?>
