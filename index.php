e34d2e9146f75a14d44a407c6a98db21172d31ed
M.Rizwan <rizwan02091986@gmail.com>
2024-03-07 20:04:07 +0500

    07-03-2024

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+    $name = "Hello Bhai";       //string
+    $age = 50;                  //int
+    $isCnic = true;             //boolean
+    $marks = 80.65;             //float
+
+    echo "$name";
+    echo "<br> $age";
+    echo "<br> $isCnic";
+    echo "<br> $marks";
+
+    // artihmetic operator
+
+    $a = 20;
+    $b = 30;
+    $c = $a + $b;
+    $d = $a - $b;
+    $e = $a * $b;
+    $f = $a / $b;
+    $g = $a % $b;
+
+    echo "<br> the sum of $a + $b is :" .$c;
+    echo "<br> the sub of $a - $b is :" .$d;
+    echo "<br> the mul of $a * $b is :" .$e;
+    echo "<br> the div of $a / $b is :" .$f;
+    echo "<br> the mod of $a % $b is :" .$g;
+
+?>
