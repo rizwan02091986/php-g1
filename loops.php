d0fd66d97e8920a28f3ddf559041224bd5cea135
M.Rizwan <rizwan02091986@gmail.com>
2024-03-07 20:33:43 +0500

    07-03-2024

---
+++ loops.php
@@ -0,0 +1,18 @@
+<?php
+
+    //loops
+    //for loop
+
+    for($i=0; $i<=10; $i++){
+        echo "<br> this is for loop" .$i;
+    }
+
+    for($q=1; $q<=10; $q++){
+        echo "<br> 19 X $q =" .$q*19;
+    }
+    $tableNum = 64;
+    for($w=1; $w<=10; $w++){
+        echo "<br> $tableNum X $w =" .$tableNum*$w;
+    }
+
+?>
