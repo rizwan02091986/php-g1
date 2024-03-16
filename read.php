874a96e7bee1c573307b94882e54ea54ae4c8450
M.Rizwan <rizwan02091986@gmail.com>
2024-03-16 17:33:31 +0500

    16-03-2024

--- read.php
+++ read.php
@@ -12,7 +12,13 @@
 
     }
     
-    if($result->num_rows)
+    if($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            echo "<pre>";
+            print_r($row);
+            echo "</pre>";
+        }
+    }
 
 
 ?>
