2a14bef3c472a22d91180f6b5e9b2c25dd21f8c6
M.Rizwan <rizwan02091986@gmail.com>
2024-03-28 17:26:09 +0500

    28-03-2024

---
+++ file/file_upload.php
@@ -0,0 +1,14 @@
+<?php
+
+    include("../config/one.php");
+    
+?>
+
+<div class="container">
+    <form action="" method="POST" enctype="multipart/form-data">
+        <div class="form-grp">
+            <input type="file" name="image" id=""> 
+            <input type="submit" value="submit" name="submit">
+        </div>
+    </form>
+</div>
