e96c30bdf3fd7d946f4b6cb9aa4f3bf454035ded
M.Rizwan <rizwan02091986@gmail.com>
2024-03-28 17:03:36 +0500

    28-03-2024

--- c_read.php
+++ c_read.php
@@ -6,10 +6,15 @@
 
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+<style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
 <div class="container">
-    <table class="table">
-        <tr>
+    <table class="table table-bordered text-white text-center">
+        <tr class="bg-primary">
             <th>Id</th>
             <th>Name</th>
             <th>Email</th>
@@ -24,7 +29,8 @@
                 <td>".$rows['name']."</td>
                 <td>".$rows['email']."</td>
                 <td>".$rows['password']."</td>
-                       
+                <td><a href='c_update.php?id=".$rows['id']."'class='btn btn-success'>Edit</a></td>
+                <td><a href='c_delete.php?id=".$rows['id']."'class='btn btn-danger'>Delete</a></td>
                 </tr>";
             }
         ?>
